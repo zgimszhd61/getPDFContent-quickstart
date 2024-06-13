0a1bbbdf8aa0658ce7995ca3b6c68a91eea00e59
xiaoming <lininruc@gmail.com>
2024-06-13 14:16:47 +0800

    add mvs

--- downloadAndpdf2text.py
+++ downloadAndpdf2text.py
@@ -7,9 +7,17 @@ from PyPDF2 import PdfReader
 def realtimeQuestion():
    pass
 
+def save_string_to_file(string_variable):
+    try:
+        with open("ooo.txt", "w") as file:
+            file.write(string_variable)
+        print("String successfully written to ooo.txt")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 ## 下载
 def mainv2():
-    url = "https://arxiv.org/pdf/2405.17372"
+    url = "https://arxiv.org/pdf/2406.06559"
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
@@ -23,10 +31,22 @@ def mainv2():
     pdf_file = requests.get(url, headers=headers)
     with open('output.pdf', 'wb') as f:
         f.write(pdf_file.content)
+    allcontent = ""
 
     reader = PdfReader('output.pdf')
     for item in reader.pages:
         page = item
         text = page.extract_text()
-        print(text)
+        # print(text)
+        allcontent = allcontent + text.split("†")[0].split("∗Correspondence")[0]
+    if "Abstract" in allcontent:
+        allcontent = allcontent.split("Abstract")[1]
+    
+    print("论文地址:{}".format(url))
+    if ("interning" in allcontent):
+        print("实习生作品，不用看")
+    else:
+        result = allcontent.split("References")[0].split("Acknowledgement")[0]
+        print(result)
+        save_string_to_file(result)
 mainv2()
--- download_pdf.py
+++ download_pdf.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://static.sse.com.cn/disclosure/listedinfo/announcement/c/new/2024-05-27/603037_20240527_QEJI.pdf"
+url = "https://arxiv.org/pdf/2406.06608"
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
